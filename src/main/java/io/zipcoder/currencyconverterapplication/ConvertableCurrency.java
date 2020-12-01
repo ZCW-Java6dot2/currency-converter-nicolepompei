9dd103f343be2bcd7a8f865c837ea16d6f6792ce
pompy <nicole.lynn.pompei@gmail.com>
2020-11-30 20:36:05 -0500

    currency converter lab

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -1,7 +1,14 @@
 package io.zipcoder.currencyconverterapplication;
 
+import java.util.Currency;
+
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
-        return Double.MAX_VALUE;
+        Double currencyToConvert = currencyType.getRate();
+        Double currencyBaseValue = this.getType().getRate();
+
+        return currencyToConvert / currencyBaseValue;
     }
+
+    CurrencyType getType();
 }
